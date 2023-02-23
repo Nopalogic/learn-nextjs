beac2c8d849a628ef9d1deaa9dbdedb69396f037
Nopalogic <naufaladhi02@gmail.com>
2023-02-23 14:08:05 +0700

    modify

--- app/page.jsx
+++ app/page.jsx
@@ -4,13 +4,13 @@ export default function Home() {
 	return (
 		<div className={style.container}>
 			<div className={style.column}>
-				<div className={style.badge}>
+				<div className={style.badge}>	
 					<span>application</span>
 				</div>
 				<div>
 					<h1 className={style.title}>Business Travel <span className={style.bar}>Management</span> Solutions.</h1>
 					<p className={style.subtitle}>
-						a universal solution for <span className={style.bold}>traveling costumers</span> and companies providing comperhensive travel services around the world.
+						A universal solution for <span className={style.bold}>traveling costumers</span> and companies providing comperhensive travel services around the world.
 					</p>
 					<button className={style.button}>
 						<span>get started</span> <span>icon</span>
