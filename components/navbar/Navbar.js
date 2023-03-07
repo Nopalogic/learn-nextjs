ef33895a987566b7cd7a71005f2e1e7ba593c6f6
Nopalogic <naufaladhi02@gmail.com>
2023-03-07 20:51:49 +0700

    refactor header & footer

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,17 +1,20 @@
 import Link from 'next/link';
-import style from './navbar.module.css'
+import style from './navbar.module.css';
 
 export default function Navbar() {
 	return (
-			<nav className={style.nav}>
+		<nav className={style.nav}>
+			<div className={style.container}>
 				<div className={style.brand}>
 					<Link href={'/'}>BRAND</Link>
 				</div>
 				<div className={style.links}>
-					<Link href={'/'}>About</Link>
+					<Link href={'/'}>Home</Link>
+					<Link href={'/about'}>About</Link>
 					<Link href={'/'}>Portfolio</Link>
 					<Link href={'/'}>Services</Link>
 				</div>
-			</nav>
+			</div>
+		</nav>
 	);
 }
